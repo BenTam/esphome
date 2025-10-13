a9fb19308fba1281a3c992bb5d12cc7dc53744b1
BenTam <benko.tamas@gmail.com>
2025-10-13 22:30:18 +0200

    Update growatt_solar.cpp

--- esphome/components/growatt_solar/growatt_solar.cpp
+++ esphome/components/growatt_solar/growatt_solar.cpp
@@ -105,8 +105,8 @@ void GrowattSolar::on_modbus_data(const std::vector<uint8_t> &data) {
       publish_1_reg_sensor_state(this->pvs_[0].voltage_sensor_, 3, ONE_DEC_UNIT);
       publish_1_reg_sensor_state(this->pvs_[0].current_sensor_, 4, ONE_DEC_UNIT);
       publish_2_reg_sensor_state(this->pvs_[0].active_power_sensor_, 5, 6, ONE_DEC_UNIT);
-      publish_2_reg_sensor_state(this->pvs_[0].today_production_, 59, 60, ONE_DEC_UNIT);
-      publish_2_reg_sensor_state(this->pvs_[0].total_energy_production_, 61, 62, ONE_DEC_UNIT);      
+      publish_2_reg_sensor_state(this->pvs_[0].today_production_sensor_, 59, 60, ONE_DEC_UNIT);
+      publish_2_reg_sensor_state(this->pvs_[0].total_energy_production_sensor_, 61, 62, ONE_DEC_UNIT);      
 
       publish_1_reg_sensor_state(this->pvs_[1].voltage_sensor_, 7, ONE_DEC_UNIT);
       publish_1_reg_sensor_state(this->pvs_[1].current_sensor_, 8, ONE_DEC_UNIT);
