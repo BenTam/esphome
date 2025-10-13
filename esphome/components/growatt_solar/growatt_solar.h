3a203d42bad2057ac5b61fe830a0af39bc206a23
BenTam <benko.tamas@gmail.com>
2025-10-13 22:15:08 +0200

    Update growatt_solar.h

--- esphome/components/growatt_solar/growatt_solar.h
+++ esphome/components/growatt_solar/growatt_solar.h
@@ -54,11 +54,11 @@ class GrowattSolar : public PollingComponent, public modbus::ModbusDevice {
 //  void set_active_power_sensor_pv(uint8_t pv, sensor::Sensor *active_power_sensor) {
 //    this->pvs_[pv].active_power_sensor_ = active_power_sensor;
 //  }
-  void set_today_production_sensor_pv(uint8_t pv, sensor::Sensor *today_production_sensor) {
-    this->pvs_[pv].today_production_sensor_ = today_production_sensor;
+  void set_today_production_pv(uint8_t pv, sensor::Sensor *today_production) {
+    this->pvs_[pv].today_production_ = today_production;
   }
-  void set_total_energy_production_sensor_pv(uint8_t pv, sensor::Sensor *total_energy_production_sensor) {
-    this->pvs_[pv].total_energy_production_sensor_ = total_energy_production_sensor;
+  void set_total_energy_production_pv(uint8_t pv, sensor::Sensor *total_energy_production) {
+    this->pvs_[pv].total_energy_production_ = total_energy_production;
   }
 
  protected:
